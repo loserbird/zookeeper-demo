2e99f629b3983a9575ef3e67118c51a4edb75f06
loserbird <15626140150@163.com>
2018-04-24 21:20:26 +0800

    first practise

---
+++ src/main/java/com/loserbird/zookeeper_demo/ChildrenCallBack.java
@@ -0,0 +1,18 @@
+package com.loserbird.zookeeper_demo;
+
+import java.util.List;
+
+import org.apache.zookeeper.AsyncCallback.ChildrenCallback;
+
+public class ChildrenCallBack implements ChildrenCallback {
+
+	@Override
+	public void processResult(int rc, String path, Object ctx, List<String> children) {
+		for (String s : children) {
+			System.out.println(s);
+		}
+		System.out.println("ChildrenCallback:" + path);
+		System.out.println((String)ctx);	
+	}
+
+}
